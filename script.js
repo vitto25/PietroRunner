b84671a7ebe22b8fe84a3f88e165f9fb641ccffd
vitto25 <vittorio.iodice@mail.polimi.it>
2024-09-19 23:16:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ let pietro = document.getElementById("pietro");
 let gameContainer = document.getElementById("gameContainer");
 let isJumping = false;
 
+// Aggiungi il listener per la tastiera (barra spaziatrice)
 document.addEventListener("keydown", function(event) {
     if (event.code === "Space" && !isJumping) {
         jump();
     }
 });
 
+// Aggiungi il listener per il click e il tocco (mobile)
+gameContainer.addEventListener("click", function() {
+    if (!isJumping) {
+        jump();
+    }
+});
+
+// Funzione che gestisce il salto
 function jump() {
     let position = 0;
     isJumping = true;
@@ -30,6 +39,7 @@ function jump() {
     }, 20);
 }
 
+// Controllo delle collisioni
 let obstacle = document.getElementById("obstacle");
 let checkCollision = setInterval(function() {
     let pietroRect = pietro.getBoundingClientRect();
